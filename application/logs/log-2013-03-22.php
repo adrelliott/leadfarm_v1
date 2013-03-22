4d63f85b47b11ff6b1db0412907add4e4af01356
Al Elliott <al@dallasmatthews.co.uk>
2013-03-22 08:25:06 +0000

    about to merge charles wilson amendments inot head

--- application/logs/log-2013-03-22.php
+++ application/logs/log-2013-03-22.php
@@ -2,3 +2,6 @@
 
 ERROR - 2013-03-22 08:18:50 --> Severity: Notice  --> mysql_pconnect(): send of 5 bytes failed with errno=10053 An established connection was aborted by the software in your host machine.
  C:\wamp\www\projects\_leadfarm\_Current_version\Leadfarm_v1\CI_Framework\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2013-03-22 08:23:03 --> Severity: Notice  --> Undefined index: content-type C:\wamp\www\projects\_leadfarm\_Current_version\Leadfarm_v1\application\libraries\infusionsoft\xmlrpc-3.0\lib\xmlrpc.inc 2559
+ERROR - 2013-03-22 08:23:15 --> Severity: Notice  --> mysql_pconnect(): send of 5 bytes failed with errno=10053 An established connection was aborted by the software in your host machine.
+ C:\wamp\www\projects\_leadfarm\_Current_version\Leadfarm_v1\CI_Framework\database\drivers\mysql\mysql_driver.php 91
