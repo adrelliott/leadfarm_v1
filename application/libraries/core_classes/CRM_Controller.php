0863bab88367b9c83f1e1b63bb846fa435dcd467
Al <user@Users-MacBook-Pro-2.local>
2013-04-03 15:27:30 +0100

    Part2

--- application/libraries/core_classes/CRM_Controller.php
+++ application/libraries/core_classes/CRM_Controller.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class MY_Controller extends CI_Controller {
+class CRM_Controller extends CI_Controller {
 
 	/**
 	 * This acts as a template for every controller.
--- application/libraries/core_classes/CRM_Model.php
+++ application/libraries/core_classes/CRM_Model.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class MY_Model extends CI_Model {
+class CRM_Model extends CI_Model {
     
      /**
      * The database table to use.
