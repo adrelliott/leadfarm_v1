b259eb59cf5a91927f693abfe947ef24eb9c078c
Al <user@Als-MacBook-Pro.local>
2013-04-23 12:55:59 +0100

    Made changes for FCUtd

--- application/views/custom/22232/contactaction/v_contactaction_edit_role.php
+++ application/views/custom/22232/contactaction/v_contactaction_edit_role.php
@@ -13,7 +13,19 @@
                                 <?php //echo display_field($fields['Id'], array('type' => 'hidden'));  ?>
                                 <?php //echo display_field($fields['ContactId'], array('type' => 'hidden', 'value' => $ContactId));  ?>
                                 <?php echo display_field($fields['ActionType'], array('type' => 'hidden', 'value' => 'Role')); ?>
-                                <?php echo display_field($fields['_ActionSubtype'], array('label' => 'Role', 'type' => 'select', 'options' => array('' => '', 'Player' => 'Player', 'Volunteer' => 'Volunteer', 'Staff' => 'Staff', 'give me your other options!' => '1'))); ?>
+                                <?php echo display_field($fields['_ActionSubtype'], array('label' => 'Role', 'type' => 'select', 'options' => array(
+                                    '' => '', 
+                                    'Volunteer (Office)' => 'Volunteer (Office)', 
+                                    'Volunteer (Matchday)' => 'Volunteer (Matchday)', 
+                                    'Paid Office Staff' => 'Paid Office Staff', 
+                                    'Community Staff' => 'Community Staff', 
+                                    'Board Member' => 'Board Member', 
+                                    'External Consultant' => 'External Consultant', 
+                                    'Coaching Staff' => 'Coaching Staff', 
+                                    '1st Team Player' => '1st Team Player', 
+                                    'Youth Team Player' => 'Youth Team Player', 
+                                    'Women\'s Team Player' => 'Women\'s Team Player',
+                                    ))); ?>
                                 <?php display_field($fields['ActionDescription'], array( 'label' => 'Full description')); ?>
                                 <?php echo display_field($fields['CreationNotes'], array('label' => 'Notes')); ?>
                                 <?php echo display_field($fields['_ValidUntil'], array('label' => 'Season')); ?>
